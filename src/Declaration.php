0a3fdede6b496600ee9b96fd85dbcc5364e3f9ea
Peter Mein <peter@infratron.io>
2020-10-22 15:40:02 +0200

    feat: added const IBAN
    feat: added formatting function for a declaration

--- src/Declaration.php
+++ src/Declaration.php
@@ -7,10 +7,9 @@ namespace Mijnkantoor\Belastingdienst;
 
 class Declaration
 {
+    public const IBAN = 'NL86INGB0002445588';
     public $declarationId;
-
     public $paymentReference;
-
     public $paymentDueDate;
 
     public function __construct($declarationId, $paymentReference, $paymentDueDate)
@@ -19,5 +18,22 @@ class Declaration
         $this->declarationId = $declarationId;
         $this->paymentReference = $paymentReference;
         $this->paymentDueDate = $paymentDueDate;
+    }
+
+    public function getFormattedIban(): string
+    {
+        return sprintf('%2s %2d %4s %4s %4s %2s',
+            substr(self::IBAN, 0, 2),
+            substr(self::IBAN, 2, 2),
+            substr(self::IBAN, 4, 4),
+            substr(self::IBAN, 8, 4),
+            substr(self::IBAN, 12, 4),
+            substr(self::IBAN, 16, 2));
+
+    }
+
+    public function getFormattedPaymentReference(): string
+    {
+        return implode('.', str_split($this->paymentReference, 4));
     }
 }
